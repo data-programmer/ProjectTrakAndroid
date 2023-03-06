80c651b90077428fbca1f3b1f04ecc9ea764471d
William <williamkingsley2019@outlook.com>
2023-03-06 12:21:38 -0700

    Implementing detail screen

--- projects/src/main/java/com/kingsland/projects/presentation/detail/component/ProjectDetailLoaded.kt
+++ projects/src/main/java/com/kingsland/projects/presentation/detail/component/ProjectDetailLoaded.kt
@@ -1,16 +1,36 @@
 package com.kingsland.projects.presentation.detail.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kingsland.core.ui.components.ButtonLayout
 import com.kingsland.projects.presentation.model.Project
 
 @Composable
 fun ProjectDetailLoaded(
+    modifier: Modifier = Modifier,
     project: Project,
     onProjectEdit: () -> Unit = { },
     onProjectDelete: () -> Unit = { }
 ) {
+    ButtonLayout(
+        buttonIcon = Icons.Filled.Edit,
+        onButtonClick = onProjectEdit
+    ) {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
+        }
+    }
 }
 
 @Composable
